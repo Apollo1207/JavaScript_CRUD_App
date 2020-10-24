ed1f510b02714e831768a0f1e56ca84486663b31
Apollo1207 <fednyakillya12072001@gmail.com>
2020-10-24 19:08:39 +0300

    refactoring and fixes bugs, other improvements

--- js/script.js
+++ js/script.js
@@ -1,88 +1,52 @@
-// var x = document.getElementById("a").innerHTML;
-// console.log(x);
+var x = document.getElementById("first_card_seats").innerHTML;
+var y = document.getElementById("second_card_seats").innerHTML;
+var z = document.getElementById("third_card_seats").innerHTML;
+var k = document.getElementById("second_card_seats").innerHTML;
+var arraySeats = [x,y,z,k];
 
-// var y = document.getElementById("b").innerHTML;
-// console.log(y);
+function seatsCounter(arr){
+var sum = 0;
+for(var i = 0; i < arraySeats.length; i++){
+    sum += parseInt(arraySeats[i]);
+    }
+document.getElementById("demo").innerHTML = sum;
+}
 
-// var arr = [x,y];
-
-// function arraySum(arr){
-// var sum = 0;
-// for(var i = 0; i < arr.length; i++){
-//     sum += parseInt(arr[i]);
-//     }
-// document.getElementById("demo").innerHTML = sum;
-// }
-
-// var numArray = [140000, 104, 99];
-// numArray.sort(function(a, b) {
-//   return a - b;
-// });
-
-// console.log(numArray);
-
-
-
-
-
-
-
-// var values = document.querySelector("span");
-// console.log(values.value)
-
-
-
-
-
-
-
-function myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
+function searchFunction() {
+    let input, filter, myCards, card, location, i, txtValue;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByClassName("card");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByClassName("location")[0];
-        txtValue = a.textContent || a.innerText;
+    myCards = document.getElementById("myCards");
+    card = myCards.getElementsByClassName("card");
+    for (i = 0; i < card.length; i++) {
+        location = card[i].getElementsByClassName("location")[0];
+        txtValue = location.textContent || location.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
+            card[i].style.display = "";
         } else {
-            li[i].style.display = "none";
+            card[i].style.display = "none";
         }
     }
 }
 
-
-
-
-function sortUsingNestedText(parent, childSelector, keySelector) {
+function sortFunction(parent, childSelector, keySelector) {
     var items = parent.children(childSelector).sort(function(a, b) {
         var vA = $(keySelector, a).text();
         var vB = $(keySelector, b).text();
         vA = parseFloat(vA)
         vB = parseFloat(vB)
-        return (vA > vB) ? -1 : (vA > vB) ? 1 : 0;
-    });
+        return (vA > vB) ? -1 : (vA < vB) ? 1 : 0;
+    }); 
     parent.append(items);
 }
 
+$('#sScale').data("sortKey", "span.scale");
 
-$('#sPrice').data("sortKey", "span.scale");
+$(document).ready(function() {
+    $('input[type="checkbox"]').click(function() {
+        if ($(this).prop("checked") === true) {
+            sortFunction($('.row'), "div", $(this).data("sortKey"));
 
-
-
- $(document).ready(function(){
-        $('input[type="checkbox"]').click(function(){
-            if($(this).prop("checked") == true){
-    sortUsingNestedText($('.row'), "div", $(this).data("sortKey"));
-               
-            }
-            else if($(this).prop("checked") == false){
-               sortUsingNestedText($('.row'), "div", $(this).data("sortKey"));
-               
-              
-            }
-        });
+        }
     });
-
+});
